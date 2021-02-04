cc4e5e7defd9c17f90d9a6eb6da08ff1c91f46f7
Amanda Mattleman <amandamattleman@MSI>
2021-02-04 13:38:42 -0700

    css ready

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import hamburger from '../Media/hamburger.png'
+
+class Header extends Component{
+    constructor(){
+        super();
+        this.state = {
+            toggleShow: false
+        }
+    }
+
+    toggleShowFunc = () => {
+        this.setState( (prevState) => {
+            return{
+                toggleShow: !prevState.toggleShow
+            }
+        })
+    }
+
+    render(){
+        return <header>
+            <div>
+            <h1 className="start">Start Bootstrap</h1>
+            <nav className={`nav-bar ${this.state.toggleShow ? "show" : ""}`}>
+                <ul>
+                    <li>
+                        <a>About</a>
+                    </li>
+                    <li>
+                        <a>Projects</a>
+                    </li>
+                    <li>
+                        <a>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+            <input onClick={this.toggleShowFunc} alt="menu-icon" type="image" src={hamburger} id="nav-btn"/>
+            </div>
+        </header>
+    }
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+
+    return(
+        <main>
+            <div className="title">
+                GRAYSCALE
+            </div>
+            <p>A free, responsive, one page Bootstrap theme
+                <br></br>
+                created by Bootstrap.</p>
+                <button className="main-btn">GET STARTED</button>
+        </main>
+    )
+}
+
+export default Main;
